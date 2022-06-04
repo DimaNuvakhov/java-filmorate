88ad7d3f91ca575e805de3add8ee3673a6ae9f0e
Dima Nuvakhov <d.nuvakhov16@mail.ru>
2022-06-04 23:05:25 +0300

    4 задание, 1 версия.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -45,3 +45,9 @@ user_id integer,
 friend_id integer,
 is_accepted boolean
 );
+
+insert into rating (id, val, comm) values (1, 'G', 'У фильма нет возрастных ограничений');
+insert into rating (id, val, comm) values (2, 'PG', 'детям рекомендуется смотреть фильм с родителями';
+insert into rating (id, val, comm) values (3, 'PG-13', 'детям до 13 лет просмотр не желателен');
+insert into rating (id, val, comm) values (4, 'R', 'лицам до 17 лет просматривать фильм можно только в присутствии');
+insert into rating (id, val, comm) values (5, 'NC-17', 'лицам до 18 лет просмотр запрещён');
